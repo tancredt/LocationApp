f69eeb38ff7c386bcb703035af96aae14d675ddb
tancredt <nicholasrperkins@gmail.com>
2026-02-12 14:04:05 +1100

    fixing nginx

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
+    base: '/changelocation/',
+    plugins: [
     vue(),
   ],
   resolve: {
